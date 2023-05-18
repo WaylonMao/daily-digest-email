bb5bdf28f21fc22c9afdd5996ba87c1b26839cb4
William Mao <tomcat_mao@hotmail.com>
2023-05-18 17:28:29 -0600

    Finished get_random_quote().

--- dd_content.py
+++ dd_content.py
@@ -1,5 +1,36 @@
+import requests
+import random
+
+
+# Retrieve a random quote from https://randomwordgenerator.com/motivational-quote.php
 def get_random_quote():
-    pass
+    url = "https://randomwordgenerator.com/json/inspirational-quote_ws.json"
+
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200 and "data" in data:
+        quotes = data["data"]
+        new_quotes = []
+
+        for quote_data in quotes:
+            quote_text = quote_data["inspirational_quote"]
+
+            start_index = quote_text.index("“") + 1
+            end_index = quote_text.index("” -")
+            quote = quote_text[start_index:end_index].strip()
+
+            author_start_index = quote_text.index("<small><i>") + 10
+            author_end_index = quote_text.index("</i></small>", author_start_index)
+            author = quote_text[author_start_index:author_end_index].strip()
+
+            new_quote = {"quote": quote, "author": author}
+            new_quotes.append(new_quote)
+            return random.choice(new_quotes)
+
+    else:
+        print("No quotes found.")
+
+    return None
 
 
 def get_weather_forecast():
@@ -15,4 +46,4 @@ def get_wikipedia_article():
 
 
 if __name__ == '__main__':
-    pass  # test code
+    print(get_random_quote())
---
+++ quotes.csv
@@ -0,0 +1,11 @@
+Confucius|Never give a sword to a man who can't dance.
+Confucius|Imagination is more important than knowledge.
+Confucius|The man who says he CAN and the man who says he CANNOT are both correct.
+Confucius|The man who asks a question is a fool for a minute. The man who does not ask is a fool for life.
+Oscar Wilde|Be yourself; everyone else is already taken.
+Oscar Wilde|The truth is rarely pure and never simple.
+Sun Tzu|Appear weak when you are strong and strong when you are weak.
+Sun Tzu|The supreme art of war is to subdue the enemy without fighting.
+Sun Tzu|The greatest victory is that which requires no battle.
+Winston Churchill|Success consists of going from failure to failure without loss of enthusiasm.
+Winston Churchill|Success is not final. Failure is not fatal. It is the courage to continue that counts.
